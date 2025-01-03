71ac7891bfb38cabe89fa9fe7f351cbf82403dc0
l.quenel <l.quenel@evolucare.com>
2025-01-03 11:23:55 +0100

    first commit

---
+++ src/stories/button.component.ts
@@ -0,0 +1,97 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { LucideAngularModule, Home } from 'lucide-angular';
+
+@Component({
+  selector: 'storybook-button',
+  standalone: true,
+  imports: [CommonModule, LucideAngularModule],
+  template: ` <button
+    type="button"
+    (click)="onClick.emit($event)"
+    [ngClass]="classes"
+    [ngStyle]="{}"
+  >
+    @if(withIcon){
+    <lucide-icon
+      [img]="icon"
+      class="button-icon"
+      [size]="iconSize"
+    ></lucide-icon>
+    } @if(withLabel) {
+    {{ label }}
+    }
+  </button>`,
+  styleUrls: ['./button.css'],
+})
+export class ButtonComponent {
+  /**
+   * The icon to use
+   */
+  icon: any = Home;
+
+  /**
+   * Button Type
+   */
+  @Input()
+  type:
+    | 'primary'
+    | 'secondary'
+    | 'ghost'
+    | 'outline'
+    | 'destructive'
+    | 'constructive' = 'primary';
+
+  /** How large should the button be? */
+  @Input()
+  size: 'small' | 'medium' | 'large' = 'medium';
+
+  /**
+   * Whether to show an icon
+   */
+  @Input()
+  withIcon: boolean = true;
+  /**
+   * Whether to show a label
+   */
+  @Input()
+  withLabel: boolean = true;
+
+  /**
+   * Button contents
+   *
+   * @required
+   */
+  @Input()
+  label = 'Button';
+
+  /** Optional click handler */
+  @Output()
+  onClick = new EventEmitter<Event>();
+
+  public get iconSize(): string {
+    switch (this.size) {
+      case 'small':
+        return '14';
+      case 'medium':
+        return '16';
+      case 'large':
+        return '22';
+      default:
+        return '16';
+    }
+  }
+
+  public get classes(): string[] {
+    let mode = `storybook-button--${this.type}`;
+
+    let paddingSize = this.withLabel ? '' : 'storybook-button--without-label';
+
+    return [
+      'storybook-button',
+      `storybook-button--${this.size}`,
+      mode,
+      paddingSize,
+    ];
+  }
+}
---
+++ src/stories/button.stories.ts
@@ -0,0 +1,123 @@
+import type { Meta, StoryObj } from '@storybook/angular';
+import { fn } from '@storybook/test';
+
+import { ButtonComponent } from './button.component';
+
+export enum ButtonType {
+  PRIMARY = 'primary',
+  SECONDARY = 'secondary',
+  OUTLINE = 'outline',
+  GHOST = 'ghost',
+  DESTRUCTIVE = 'destructive',
+  CONSTRUCTIVE = 'constructive',
+}
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+const meta: Meta<ButtonComponent> = {
+  title: 'Example/Button',
+  component: ButtonComponent,
+  tags: ['autodocs'],
+  argTypes: {
+    withIcon: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    type: {
+      control: {
+        type: 'select',
+        options: [
+          ButtonType.PRIMARY,
+          ButtonType.SECONDARY,
+          ButtonType.OUTLINE,
+          ButtonType.GHOST,
+          ButtonType.DESTRUCTIVE,
+          ButtonType.CONSTRUCTIVE,
+        ],
+      },
+    },
+    size: {
+      control: {
+        type: 'select',
+        options: ['small', 'medium', 'large'],
+      },
+    },
+  },
+  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  args: { onClick: fn() },
+};
+
+export default meta;
+type Story = StoryObj<ButtonComponent>;
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Primary: Story = {
+  args: {
+    label: 'Button',
+    type: ButtonType.PRIMARY,
+    size: 'medium',
+    withIcon: true,
+    withLabel: true,
+  },
+};
+
+export const Secondary: Story = {
+  args: {
+    label: 'Button',
+    type: ButtonType.SECONDARY,
+    withIcon: true,
+    withLabel: true,
+  },
+};
+
+export const Outline: Story = {
+  args: {
+    label: 'Button',
+    type: ButtonType.OUTLINE,
+    withIcon: true,
+    withLabel: true,
+  },
+};
+
+export const Ghost: Story = {
+  args: {
+    label: 'Button',
+    type: ButtonType.GHOST,
+    withIcon: true,
+    withLabel: true,
+  },
+};
+
+export const Destructive: Story = {
+  args: {
+    label: 'Button',
+    type: ButtonType.DESTRUCTIVE,
+    withIcon: true,
+    withLabel: true,
+  },
+};
+
+export const Constructive: Story = {
+  args: {
+    label: 'Button',
+    type: ButtonType.CONSTRUCTIVE,
+    withIcon: true,
+    withLabel: true,
+  },
+};
+
+export const Large: Story = {
+  args: {
+    size: 'large',
+    label: 'Button',
+    type: 'primary',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: 'small',
+    label: 'Button',
+    type: 'primary',
+  },
+};
